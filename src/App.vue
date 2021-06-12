bd984c0873834de8bddf8af72e06e60810b28ee1
berthot <matheus.berthot@gmail.com>
2021-06-12 17:51:51 -0300

    screen

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,5 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: white;
-  margin-top: 60px;
-}
+
 </style>
