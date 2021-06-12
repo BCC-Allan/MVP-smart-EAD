4d3576c30ed4063e1dfc9a53496249c2c4b4fe86
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2021-06-12 15:40:38 -0300

    starting user bar

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,19 @@
   <card/>
 
 
+  <main>
+    <h1>Smart ead</h1>
+    <UserBar ></UserBar>
+  </main>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import UserBar from "./components/UserBar.vue";
 import Card from "./components/card.vue";
 
 export default defineComponent({
-  components: {Card}
+  components: {UserBar, Card}
 });
 </script>
 
