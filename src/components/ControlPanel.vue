d5f123b645a43e244f2d4a54dcb5f21b1daed2cd
berthot <matheus.berthot@gmail.com>
2021-06-12 15:39:05 -0300

    add component

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <h1>{{batata}}</h1>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+
+});
+</script>
+
+<style scoped>
+
+.wrapper {
+}
+</style>
