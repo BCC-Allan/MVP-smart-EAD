bddca2e7355a9b7b9205bed91e41d0c561663f37
berthot <matheus.berthot@gmail.com>
2021-06-13 16:00:46 -0300

    agora vai

--- src/components/ClassContainer.vue
+++ src/components/ClassContainer.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="wrapper-card">
     <div class="screen-peoples">
-      <div class="people-images">
-        <ul class="peoples">
-          <li v-for="i in test" :key="i.name">
-            <img :alt="i.name" :src="i.picture" class="picture">
-            <p> {{ i.name }}</p>
-          </li>
-        </ul>
-      </div>
-      <div class="screen">
-        <div class="screen-share">
-          <img alt="" src="src/assets/test.png">
-        </div>
-        <div class="teacher">
-          <img alt="" src="src/assets/peoples/pt.jpg">
-
-        </div>
-      </div>
+      <Peoples/>
+      <ScreenShareAndTeacher/>
     </div>
     <div class="user-bar">
       <UserBar></UserBar>
@@ -30,47 +15,11 @@
 import {defineComponent} from 'vue';
 import ControlPanel from './ControlPanel.vue';
 import UserBar from './UserBar.vue'
+import Peoples from "./Peoples.vue";
+import ScreenShareAndTeacher from "./ScreenShareAndTeacher.vue";
 
 export default defineComponent({
-  components: {ControlPanel, UserBar},
-  data: function () {
-    return {
-      test: [
-        {
-          name: 'Valdemar',
-          picture: 'src/assets/peoples/p1.jpg',
-          enable: false
-        },
-        {
-          name: 'Valdemar',
-          picture: 'src/assets/peoples/p2.jpg',
-          enable: false
-        },
-        {
-          name: 'Valdemar',
-          picture: 'src/assets/peoples/p3.jpg',
-          enable: false
-        },
-        {
-          name: 'Valdemar',
-          picture: 'src/assets/peoples/p4.jpg',
-          enable: false
-        },
-        {
-          name: 'Valdemar',
-          picture: 'src/assets/peoples/p5.jpg',
-          enable: false
-        },
-        {
-          name: 'Valdemar',
-          picture: 'src/assets/peoples/p6.jpg',
-          enable: false
-        },
-
-
-      ]
-    }
-  },
+  components: {ScreenShareAndTeacher, Peoples, ControlPanel, UserBar},
 
 });
 </script>
@@ -87,40 +36,6 @@ export default defineComponent({
   width: 40%;
 }
 
-.people-images {
-  display: revert;
-  position: relative;
-  height: 20%;
-  width: 100%;
-  background-color: var(--secondary-color);
-
-}
-
-.picture {
-  border-radius: 5px;
-  margin-left: 15px;
-  height: 70px;
-  width: 70px;
-  border: black 3px solid;
-}
-
-.peoples p {
-  font-weight: bold;
-}
-
-.peoples {
-  padding-top: 10px;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-
-}
-
-
-.screen img {
-  height: 100%;
-  width: 100%;
-}
 
 .wrapper-card {
   background-color: var(--primary-color);
@@ -138,24 +53,5 @@ export default defineComponent({
 
 }
 
-.screen {
-  display: flex;
-  background-color: var(--primary-color);
-  position: relative;
-  height: 100%;
-  width: 100%;
-  padding: 30px 0 30px 30px;
-}
 
-
-.screen-share {
-}
-
-.teacher {
-  height: 50%;
-  width: 60%;
-  padding-right: 30px;
-  margin-left: 30px;
-
-}
 </style>
---
+++ src/components/Peoples.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="people-images">
+    <ul class="peoples">
+      <li v-for="i in test" :key="i.name">
+        <img :alt="i.name" :src="i.picture" class="picture">
+        <p class="people-name">{{ i.name }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  data: function () {
+    return {
+      test: [
+        {
+          name: 'Valdemar',
+          picture: 'src/assets/peoples/p1.jpg',
+          enable: false
+        },
+        {
+          name: 'Gabriela',
+          picture: 'src/assets/peoples/p2.jpg',
+          enable: false
+        },
+        {
+          name: 'Julieta',
+          picture: 'src/assets/peoples/p3.jpg',
+          enable: false
+        },
+        {
+          name: 'Allan',
+          picture: 'src/assets/peoples/p4.jpg',
+          enable: false
+        },
+        {
+          name: 'Pedro',
+          picture: 'src/assets/peoples/p5.jpg',
+          enable: false
+        },
+        {
+          name: 'Matheus',
+          picture: 'src/assets/peoples/p6.jpg',
+          enable: false
+        },
+
+
+      ]
+    }
+  },
+
+});
+</script>
+<style scoped>
+
+* {
+  list-style: none;
+}
+
+.people-images {
+  display: revert;
+  position: relative;
+  height: 20%;
+  width: 100%;
+  background-color: var(--secondary-color);
+  border-bottom: 2px solid var(--darker-color);
+
+}
+
+.picture {
+  border-radius: 5px;
+  height: 70px;
+  width: 70px;
+  border: black 2px solid;
+  margin: 0 30px;
+}
+
+.picture:first-child {
+  margin-left: 40px;
+}
+
+.peoples p {
+  font-weight: bold;
+}
+
+.peoples {
+  padding-top: 10px;
+  display: flex;
+  row-gap: 10px;
+
+  flex-direction: row;
+
+}
+
+
+.screen img {
+  height: 100%;
+  width: 100%;
+}
+
+
+.people-name {
+  margin: 0;
+  text-align: center;
+}
+</style>
